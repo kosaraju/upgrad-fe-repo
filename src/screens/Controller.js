4102fd1515c3d1a71b37ae1fef73d53fc0ca862d
kosaraju <gopalakrishna.kosaraju@gmail.com>
2020-01-26 12:51:06 +0100

    Update Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://54.208.56.254:8080/api/v1/";
+    this.baseUrl = "http://ec2-54-208-56-254.compute-1.amazonaws.com:8080/api/v1/";
   }
   render(){
     return(
